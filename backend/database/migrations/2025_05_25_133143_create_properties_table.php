5220863bc3c84bf934f5d61f56612c8b96ab8f1e
natsu240 <nsuh4442@gmail.com>
2025-05-25 22:35:08 +0900

    add:物件テーブル作成

---
+++ backend/database/migrations/2025_05_25_133143_create_properties_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('properties', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');         // 物件名
+            $table->string('address');      // 住所
+            $table->double('latitude');     // 緯度
+            $table->double('longitude');    // 経度
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('properties');
+    }
+};
