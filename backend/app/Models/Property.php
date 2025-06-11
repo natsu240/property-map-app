e21e60aed2892bf13327521bfc8fda7bc41cf013
natsu240 <nsuh4442@gmail.com>
2025-06-11 20:31:43 +0900

    update:テストコードが動くように修正

--- backend/app/Models/Property.php
+++ backend/app/Models/Property.php
@@ -3,8 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Property extends Model
 {
+    use HasFactory;
     protected $fillable = ['name', 'address', 'latitude', 'longitude'];
 }
